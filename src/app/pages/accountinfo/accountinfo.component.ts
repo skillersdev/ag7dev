e3e5cf50ab459319893e9713f92eabfe15de1df2
skillersdev <sridhar.skillers@gmail.com>
2019-02-20 01:28:31 +0530

    market manager pages chnages by manimaran

--- src/app/pages/accountinfo/accountinfo.component.ts
+++ src/app/pages/accountinfo/accountinfo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Routes,Router,RouterModule}  from '@angular/router';
+
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-accountinfo',
@@ -7,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountinfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService,private router: Router) { }
 
   ngOnInit() {
+    this.loginService.localStorageData();
+     this.loginService.viewsActivate();
   }
 
 }
