a3daea0f37a4889d943627e9c8b76545c47e223b
skillersdev <sridhar.skillers@gmail.com>
2019-02-25 01:50:46 +0530

    package api setting info by manimaran

--- src/app/appSettings.ts
+++ src/app/appSettings.ts
@@ -1,9 +1,7 @@
 export class AppSettings{
 
 	//public static API_BASE = "http://php55.development.local/php17052301_sspandian_erp/ver1/api";
-	//public static API_BASE = "http://localhost/ag7dev.git/trunk/api";
-	public static API_BASE = "http://localhost/ag7dev1/trunk/api";
-	
+	public static API_BASE = "http://localhost/ag7dev.git/trunk/api";
 	//public static API_BASE = "http://preview56.24x7a2z.com/bluberrydemo/api";
 
 	//public static API_BASE = "http://erp.buyperungayam.com/api";
@@ -11,10 +9,6 @@ export class AppSettings{
 
 	//public static testinfo = AppSettings.LOCAL_API_BASE; //dashboard api
 	public static getDashboardinfo = AppSettings.API_BASE + "/dashboard/dashboardinfo"; //dashboard api
-
-	//User Login Api Starts here
-	public static Userlogin=AppSettings.API_BASE + "/login"; //common api
-	//User Login Api Ends here
 	
 	/**Package APi list**/
 	public static Addpackage = AppSettings.API_BASE + "/addpackage"; //common api
@@ -23,4 +17,7 @@ export class AppSettings{
 	public static Updatepackage= AppSettings.API_BASE+"/updatepackage";
 	public static deletepackage=AppSettings.API_BASE+"/deletepackage/";
 	/*End */
+	//User Login Api Starts here
+	public static Userlogin=AppSettings.API_BASE + "/user/login"; //common api
+	//User Login Api Ends here
 }
