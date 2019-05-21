5664201fdd61c9459788066f6c8140a50e0faaa9
skillersdev <sridhar.skillers@gmail.com>
2019-05-22 00:21:14 +0530

    done by manimaran

---
+++ src/app/pages/managetemplates/addtemplate.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from '@angular/core';
+import { Routes,Router,RouterModule}  from '@angular/router';
+
+import { AppComponent } from '../../app.component';
+
+import { TopnavComponent } from '../../topnav/topnav.component';
+import { SidemenuComponent } from '../../sidemenu/sidemenu.component';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { AppSettings } from '../../appSettings';
+import { LoginService } from '../../services/login.service';
+import { CommonService} from '../../services/common.service';
+declare var jquery:any;
+declare var $ :any;
+import { Injectable } from '@angular/core';
+
+@Component({
+  selector: 'app-manageuser',
+  templateUrl: './addtemplate.component.html'
+})
+export class AddtemplateComponent implements OnInit {
+
+  currentUser:any;
+  currentUserID:any;
+  currentUsername:any;
+  currentUsergroup:any;
+  currentUserStatus:any;
+  currentAllUsers:any;
+  model: any = {};
+  packagelist:Array<Object>;
+  alldata: any = {};
+  getpackagelistRestApiUrl:string = AppSettings.getPackageDetail; 
+  checkUserRestApiUrl:string = AppSettings.checkuserdetail; 
+  AddUserRestApiUrl:string = AppSettings.Adduser;
+  AddTemplateRestApiUrl:string = AppSettings.Addtemplate;
+
+  constructor(private loginService: LoginService,private CommonService:CommonService,private router: Router,private http:Http) { 
+      document.body.className="theme-red";
+
+  }
+
+  ngOnInit() {
+     this.loginService.localStorageData();
+      this.loginService.viewsActivate();
+      //this.model.active=1;
+      this.CommonService.getdata(this.getpackagelistRestApiUrl)
+        .subscribe(packagedet =>{
+            if(packagedet.result!="")
+            { 
+              this.packagelist= packagedet.result;
+            } 
+            console.log(this.packagelist); 
+        });
+  }
+  
+  logout(){
+    this.loginService.logout();
+  }
+
+  back(){
+    this.router.navigate(['/manageuser']);
+  }
+  addtemplate()
+  {
+    this.CommonService.insertdata(this.AddTemplateRestApiUrl,this.model)
+    .subscribe(package_det =>{       
+         swal(
+          package_det.status,
+          package_det.message,
+          package_det.status
+        )
+        this.router.navigate(['/managetemplates']); 
+    });
+  }
+  checkUserexist(event:any)
+  {
+    //console.log(event.target.value);
+    this.model.username=event.target.value;
+    this.CommonService.checkexistdata(this.checkUserRestApiUrl,this.model)
+    .subscribe(package_det =>{
+      if(package_det.exist==1)
+      {
+        swal('',package_det.message,'error')
+      }  
+    });
+  }
+
+}
---
+++ src/app/pages/managetemplates/managetemplate.component.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit } from '@angular/core';
+import { Routes,Router,RouterModule}  from '@angular/router';
+
+import { AppComponent } from '../../app.component';
+
+import { TopnavComponent } from '../../topnav/topnav.component';
+import { SidemenuComponent } from '../../sidemenu/sidemenu.component';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { AppSettings } from '../../appSettings';
+import { LoginService } from '../../services/login.service';
+import { CommonService} from '../../services/common.service';
+declare var jquery:any;
+declare var $ :any;
+import { Injectable } from '@angular/core';
+
+@Component({
+  selector: 'app-manageuser',
+  templateUrl: './managetemplate.component.html',
+  //styleUrls: ['./managetemplate.component.css']
+})
+export class ManagetemplateComponent implements OnInit {
+
+  currentUser:any;
+  currentUserID:any;
+  currentUsername:any;
+  currentUsergroup:any;
+  currentUserStatus:any;
+  currentAllUsers:any;
+  model: any = {};
+  alldata: any = {};
+  userlist:Array<Object>;
+  gettemplatelistRestApiUrl:string=AppSettings.gettemplatelist;
+  DeleteuserRestApiUrl:string = AppSettings.deleteuser; 
+  constructor(
+    private loginService: LoginService,
+    private CommonService:CommonService,
+    private router: Router,
+    private http:Http) { 
+      document.body.className="theme-red";
+
+  }
+
+  ngOnInit() {
+    this.loginService.localStorageData();
+    this.loginService.viewsActivate();
+    this.CommonService.getdata(this.gettemplatelistRestApiUrl)
+    .subscribe(userdet =>{
+        if(userdet)
+        { 
+          this.userlist= userdet;
+        }
+        console.log(this.userlist); 
+         
+    });
+  }
+  
+  logout(){
+    this.loginService.logout();
+  }
+
+  navigateAddtemplate()
+  {
+    this.router.navigate(['/addtemplate']);
+  }
+  edituser(id:any)
+  {
+    this.router.navigate(['/edituser', id]);
+  }
+  deleteuser(id:any)
+ {
+   let idx = id;
+      let self = this;
+      swal({
+        title: 'Are you sure?',
+         buttons: {
+            cancel: true,
+            confirm: true,
+          },
+        text: "You won't be able to revert this!",
+      }).then(function (result) {
+        self.removeuser(idx);
+        swal(
+          'Deleted!',
+          'User Data has been deleted.',
+          'success'
+        )
+      },function(dismiss) {
+  // dismiss can be "cancel" | "close" | "outside"
+});
+ }
+ removeuser(idx:any)
+ {
+   this.CommonService.deletedata(this.DeleteuserRestApiUrl,idx)
+        .subscribe(resultdata =>{
+           this.CommonService.getdata(this.gettemplatelistRestApiUrl)
+        .subscribe(packagedet =>{
+            if(packagedet.result!="")
+            { 
+              this.userlist= packagedet.result;
+            } 
+             
+        });
+      });
+ }
+
+}
