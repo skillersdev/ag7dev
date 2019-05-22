da81929ff5f7230196ea7ede6e6bbb42fb124b5a
skillersdev <sridhar.skillers@gmail.com>
2019-05-22 17:23:23 +0530

    language script addded by sri

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -19,7 +19,21 @@ model: any = {};
   	private router: Router,private http:Http) { }
 
   ngOnInit() {
+
+     let translate_url="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    this.loadScript(translate_url);
+
   }
+
+  public loadScript(url) {
+    console.log('preparing to load...')
+    let node = document.createElement('script');
+    node.src = url;
+    node.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(node);
+  }
+
+
    checkwebsite()
   {
     // console.log(this.model.website); 
