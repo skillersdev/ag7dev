cb8ba42c1b8437c6129acdf1c896ea80885f0636
skillersdev <sridhar.skillers@gmail.com>
2019-08-12 21:02:26 +0530

    template added

---
+++ website/application/views/gallery1.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <title>Roodabatoz</title>
+        <meta name="description" content="Roodab Image Gallery"/>
+        <meta name="keywords" content="roodabatoz, gallery, image gallery, album"/>
+        <meta name="author" content="Sridhar"/>
+        <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<script src="<?php echo base_url();?>assets/gallery1/js/modernizr.custom.70736.js"></script>
+		<noscript><link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/gallery1/css/noJS.css"/></noscript>
+		<!--[if lte IE 7]><style>.main{display:none;} .support-note .note-ie{display:block;}</style><![endif]-->
+    </head>
+    <body>
+        <div class="container">
+		
+						
+			<div class="main">
+				<header class="clearfix">
+				
+					<h1>Album<span>Name comes here</span></h1>
+
+					<div class="support-note">
+						<span class="note-ie">Sorry, only modern browsers.</span>
+					</div>
+					
+				</header>
+				
+				<div class="gamma-container gamma-loading" id="gamma-container">
+
+					<ul class="gamma-gallery">
+
+						<li>
+							<div data-alt="img03" data-description="<h3>Sky high</h3>" data-max-width="1800" data-max-height="1350">
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/xxxlarge/3.jpg" data-min-width="1300"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/xxlarge/3.jpg" data-min-width="1000"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/xlarge/3.jpg" data-min-width="700"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/large/3.jpg" data-min-width="300"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/medium/3.jpg" data-min-width="200"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/small/3.jpg" data-min-width="140"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/xsmall/3.jpg"></div>
+								<noscript>
+									<img src="<?php echo base_url();?>assets/gallery1/images/xsmall/3.jpg" alt="img03"/>
+								</noscript>
+							</div>
+						</li>
+						<li>
+							<div data-alt="img03" data-description="<h3>Sky high</h3>" data-max-width="1800" data-max-height="1350">
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/xxxlarge/3.jpg" data-min-width="1300"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/xxlarge/3.jpg" data-min-width="1000"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/xlarge/3.jpg" data-min-width="700"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/large/3.jpg" data-min-width="300"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/medium/3.jpg" data-min-width="200"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/small/3.jpg" data-min-width="140"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/xsmall/3.jpg"></div>
+								<noscript>
+									<img src="<?php echo base_url();?>assets/gallery1/images/xsmall/3.jpg" alt="img03"/>
+								</noscript>
+							</div>
+						</li>
+						<li>
+							<div data-alt="img03" data-description="<h3>Sky high</h3>" data-max-width="1800" data-max-height="1350">
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/xxxlarge/3.jpg" data-min-width="1300"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/xxlarge/3.jpg" data-min-width="1000"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/xlarge/3.jpg" data-min-width="700"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/large/3.jpg" data-min-width="300"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/medium/3.jpg" data-min-width="200"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/small/3.jpg" data-min-width="140"></div>
+								<div data-src="<?php echo base_url();?>assets/gallery1/images/xsmall/3.jpg"></div>
+								<noscript>
+									<img src="<?php echo base_url();?>assets/gallery1/images/xsmall/3.jpg" alt="img03"/>
+								</noscript>
+							</div>
+						</li>
+						
+					</ul>
+
+					<div class="gamma-overlay"></div>
+
+					<div id="loadmore" class="loadmore">Loading more...</div>
+
+				</div>
+
+			</div><!--/main-->
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script src="<?php echo base_url();?>assets/gallery1/js/jquery.masonry.min.js"></script>
+		<script src="<?php echo base_url();?>assets/gallery1/js/jquery.history.js"></script>
+		<script src="<?php echo base_url();?>assets/gallery1/js/js-url.min.js"></script>
+		<script src="<?php echo base_url();?>assets/gallery1/js/jquerypp.custom.js"></script>
+		<script src="<?php echo base_url();?>assets/gallery1/js/gamma.js"></script>
+		<script type="text/javascript">
+			
+			$(function() {
+
+				var GammaSettings = {
+						// order is important!
+						viewport : [ {
+							width : 1200,
+							columns : 5
+						}, {
+							width : 900,
+							columns : 4
+						}, {
+							width : 500,
+							columns : 3
+						}, { 
+							width : 320,
+							columns : 2
+						}, { 
+							width : 0,
+							columns : 2
+						} ]
+				};
+
+				Gamma.init( GammaSettings, fncallback );
+
+
+				// Example how to add more items (just a dummy):
+
+				var page = 0,
+					items = ['<li><div data-alt="img03" data-description="<h3>Sky high</h3>" data-max-width="1800" data-max-height="1350"><div data-src="images/xxxlarge/3.jpg" data-min-width="1300"></div><div data-src="images/xxlarge/3.jpg" data-min-width="1000"></div><div data-src="images/xlarge/3.jpg" data-min-width="700"></div><div data-src="images/large/3.jpg" data-min-width="300"></div><div data-src="images/medium/3.jpg" data-min-width="200"></div><div data-src="images/small/3.jpg" data-min-width="140"></div><div data-src="images/xsmall/3.jpg"></div><noscript><img src="images/xsmall/3.jpg" alt="img03"/></noscript></div></li><li><div data-alt="img03" data-description="<h3>Sky high</h3>" data-max-width="1800" data-max-height="1350"><div data-src="images/xxxlarge/3.jpg" data-min-width="1300"></div><div data-src="images/xxlarge/3.jpg" data-min-width="1000"></div><div data-src="images/xlarge/3.jpg" data-min-width="700"></div><div data-src="images/large/3.jpg" data-min-width="300"></div><div data-src="images/medium/3.jpg" data-min-width="200"></div><div data-src="images/small/3.jpg" data-min-width="140"></div><div data-src="images/xsmall/3.jpg"></div><noscript><img src="images/xsmall/3.jpg" alt="img03"/></noscript></div></li><li><div data-alt="img03" data-description="<h3>Sky high</h3>" data-max-width="1800" data-max-height="1350"><div data-src="images/xxxlarge/3.jpg" data-min-width="1300"></div><div data-src="images/xxlarge/3.jpg" data-min-width="1000"></div><div data-src="images/xlarge/3.jpg" data-min-width="700"></div><div data-src="images/large/3.jpg" data-min-width="300"></div><div data-src="images/medium/3.jpg" data-min-width="200"></div><div data-src="images/small/3.jpg" data-min-width="140"></div><div data-src="images/xsmall/3.jpg"></div><noscript><img src="images/xsmall/3.jpg" alt="img03"/></noscript></div></li><li><div data-alt="img03" data-description="<h3>Sky high</h3>" data-max-width="1800" data-max-height="1350"><div data-src="images/xxxlarge/3.jpg" data-min-width="1300"></div><div data-src="images/xxlarge/3.jpg" data-min-width="1000"></div><div data-src="images/xlarge/3.jpg" data-min-width="700"></div><div data-src="images/large/3.jpg" data-min-width="300"></div><div data-src="images/medium/3.jpg" data-min-width="200"></div><div data-src="images/small/3.jpg" data-min-width="140"></div><div data-src="images/xsmall/3.jpg"></div><noscript><img src="images/xsmall/3.jpg" alt="img03"/></noscript></div></li><li><div data-alt="img03" data-description="<h3>Sky high</h3>" data-max-width="1800" data-max-height="1350"><div data-src="images/xxxlarge/3.jpg" data-min-width="1300"></div><div data-src="images/xxlarge/3.jpg" data-min-width="1000"></div><div data-src="images/xlarge/3.jpg" data-min-width="700"></div><div data-src="images/large/3.jpg" data-min-width="300"></div><div data-src="images/medium/3.jpg" data-min-width="200"></div><div data-src="images/small/3.jpg" data-min-width="140"></div><div data-src="images/xsmall/3.jpg"></div><noscript><img src="images/xsmall/3.jpg" alt="img03"/></noscript></div></li>']
+
+				function fncallback() {
+
+					$( '#loadmore' ).show().on( 'click', function() {
+
+						++page;
+						var newitems = items[page-1]
+						if( page <= 1 ) {
+							
+							Gamma.add( $( newitems ) );
+
+						}
+						if( page === 1 ) {
+
+							$( this ).remove();
+
+						}
+
+					} );
+
+				}
+
+			});
+
+		</script>	
+	</body>
+</html>
